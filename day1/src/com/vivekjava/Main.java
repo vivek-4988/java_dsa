dbdbf51f703ddac3fb168bbcc6dc3b2c382b00eb
vivek <vivek@creatingwinners.com>
2022-02-28 22:34:56 +0800

    pyramid pattern13

--- day1/src/com/vivekjava/Main.java
+++ day1/src/com/vivekjava/Main.java
@@ -6,7 +6,8 @@ public class Main {
         int n = 4;
         int m = 7;
 
-        pattern12(m);
+        pattern13(n);
+        //pattern12(m);
         //pattern11(m);
         //pattern10(m);
         //pattern9(m);
@@ -18,6 +19,36 @@ public class Main {
         //pattern3(n);
         //pattern2(n, m);
         //pattern1(n,m);
+    }
+
+    //dymond pattern
+    static void pattern13(int n) {
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = n-1; i >= 0; i--) {
+            for (int j = n-i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int j = i; j > 0; j--) {
+                System.out.print("*");
+            }
+            for (int j = i; j > 1; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
     }
 
     //butterfly
