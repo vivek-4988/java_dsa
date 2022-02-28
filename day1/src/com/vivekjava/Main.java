7a4d606697a2c56fae9dcf8e7567450b5d2b0003
vivek <vivek@creatingwinners.com>
2022-02-28 19:09:30 +0800

    three pattern added 4 to 6

--- day1/src/com/vivekjava/Main.java
+++ day1/src/com/vivekjava/Main.java
@@ -4,18 +4,63 @@ public class Main {
 
     public static void main(String[] args) {
         int n = 4;
-        int m = 5;
+        int m = 7;
 
-        pattern3(n);
+        pattern6(m);
+        //pattern5(m);
+        //pattern4(m);
+        //pattern3(n);
         //pattern2(n, m);
         //pattern1(n,m);
+    }
+
+    //print 1 to n as left pyramid and revert it
+    static void pattern6(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j < i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    //right pyramid
+    private static void pattern5(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
+
+    //left reverse pyramid
+    private static void pattern4(int n) {
+        for (int i = n; i > 0; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 
     //left pyramid
     private static void pattern3(int n) {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                    System.out.print("*");
+                System.out.print("*");
             }
             System.out.println();
         }
@@ -25,7 +70,7 @@ public class Main {
     private static void pattern2(int n, int m) {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
-                if (i==1||j==1||i==n||j==m)
+                if (i == 1 || j == 1 || i == n || j == m)
                     System.out.print("*");
                 else
                     System.out.print(" ");
