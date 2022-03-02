8228e4d3428f64ec924b5b7b27cd5dbf5a155245
vivek <vivek@creatingwinners.com>
2022-03-02 16:59:13 +0800

    sprialOrderMatrix

--- day2/src/com/vivekjava/Main.java
+++ day2/src/com/vivekjava/Main.java
@@ -6,7 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        sprialOrderMatrix();
+
+
+        //sprialOrderMatrix();
 
         //searchAndPrint2Darray();
 
--- day2/src/com/vivekjava/MyStrings.java
+++ day2/src/com/vivekjava/MyStrings.java
@@ -2,6 +2,43 @@ package com.vivekjava;
 
 public class MyStrings {
     public static void main(String[] args) {
+        prePostIncrement();
+    }
+
+    static void prePostIncrement() {
+        System.out.println("===================PRE POST INCREMENT=======================");
+        int a = 10;
+        int b = 5;
+
+        System.out.println("A value before a++ = " + a);
+        b = a++; // this will assign b value to 10 and then made a + 1
+        System.out.println("b = "+b);
+        System.out.println("A value after a++ = " + a);
+
+
+        System.out.println("A value before ++a = " + a);
+        b = ++a; // this will assign b value , after adding a+1
+        System.out.println("b = "+b);
+        System.out.println("A value after ++a = " + a);
+        System.out.println("===================PRE POST DECREMENT=======================");
+        a = 10; //reassign
+        //for minus
+        System.out.println("A value before a-- = " + a);
+        b = a--; // this will assign b value to 10 and then made a + 1
+        System.out.println("b = "+b);
+        System.out.println("A value after a-- = " + a);
+
+
+        System.out.println("A value before --a = " + a);
+        b = --a; // this will assign b value , after adding a+1
+        System.out.println("b = "+b);
+        System.out.println("A value after --a = " + a);
+
+
+
+    }
+
+    static void reverseString() {
         StringBuilder sb = new StringBuilder("Hello");
 
         for (int i = 0; i < sb.length() / 2; i++) {
@@ -11,9 +48,11 @@ public class MyStrings {
             char frontChar = sb.charAt(front);
             char backChar = sb.charAt(back);
 
-            sb.setCharAt(front,backChar);
-            sb.setCharAt(back,frontChar);
+            sb.setCharAt(front, backChar);
+            sb.setCharAt(back, frontChar);
         }
         System.out.println(sb);
     }
 }
+
+
