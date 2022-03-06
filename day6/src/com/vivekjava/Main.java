8c1ebfe2d67c10811f9d05e73f275548bca4d256
vivek <vivek@creatingwinners.com>
2022-03-06 20:25:02 +0800

    fibonacci in reverse

--- day6/src/com/vivekjava/Main.java
+++ day6/src/com/vivekjava/Main.java
@@ -1,17 +1,35 @@
 package com.vivekjava;
 
+import java.util.jar.JarOutputStream;
+
 public class Main {
+
+    static int sum = 0;
 
     public static void main(String[] args) {
         // write your code here
 
         //print1to5Recursive(1);
         //printSumofN(0, 8);
-        int v = printFactorial(5);
-        System.out.println(v);
+
+        //todo printFactorial
+        //int v = printFactorial(5);
+        //System.out.println(v);
+
+        //todo print fibbonacciSum
+        System.out.print("0,1,");
+        printFibbonaciSum(0, 1, 7-2);
     }
 
-    static int sum = 0;
+    //0 , 1 , 1 , 2 , 3 , 5
+    private static void printFibbonaciSum(int a, int b, int n) {
+        if (n == 0) {
+            return;
+        }
+        int c = a + b;
+        System.out.print(c + ",");
+        printFibbonaciSum(b, c, n - 1);
+    }
 
     private static int printFactorial(int n) {
         if (n == 0 || n == 1) {
@@ -20,7 +38,6 @@ public class Main {
         int fact = printFactorial(n - 1);//20
         return n * fact;
     }
-
 
     public static void printSumofN(int i, int n) {
         if (i == n) {
