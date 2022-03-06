3b615c3660e3c6e67260ff35dbf7f417b6079da7
vivek <vivek@creatingwinners.com>
2022-03-06 13:57:54 +0800

    recursive factorial

--- day6/src/com/vivekjava/Main.java
+++ day6/src/com/vivekjava/Main.java
@@ -4,9 +4,33 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        print1to5Recursive(1);
+
+        //print1to5Recursive(1);
+        //printSumofN(0, 8);
+        int v = printFactorial(5);
+        System.out.println(v);
     }
 
+    static int sum = 0;
+
+    private static int printFactorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        int fact = printFactorial(n - 1);//20
+        return n * fact;
+    }
+
+
+    public static void printSumofN(int i, int n) {
+        if (i == n) {
+            System.out.println(sum);
+            return;
+        }
+        i += 1;
+        sum += i;
+        printSumofN(i, n);
+    }
 
     public static void print1to5Recursive(int n) {
         if (n == 6) {
