3fefddc4bd84ba6bf77c4446a804e3704bf21aad
vivek <vivek@creatingwinners.com>
2022-03-09 14:37:10 +0800

    day8 with number pattern

--- day8/src/com/vivekjava/Main.java
+++ day8/src/com/vivekjava/Main.java
@@ -3,8 +3,53 @@ package com.vivekjava;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
-        p1(5);
+        p3(5);
+    }
+
+    public static void p3(int n) {
+        int number = 1;
+        int nst = 1;
+        int nsp = n - 1;
+        for (int i = 1; i <= n; i++) {
+            //spaces
+            for (int j = 1; j <= nsp; j++) {
+                System.out.print("   ");
+            }
+
+            //number 1
+            for (int j = 1; j <= nst; j++) {
+                System.out.printf(" %d ", number);
+                number++;
+            }
+
+            System.out.println();
+            nsp--;
+            nst += 2;
+
+        }
+    }
+
+    public static void p2(int n) {
+        int number = 1;
+        int nst = 1;
+        int nsp = n - 1;
+        for (int i = 1; i <= n; i++) {
+            //spaces
+            for (int j = 1; j <= nsp; j++) {
+                System.out.print("   ");
+            }
+
+            //number 1
+            for (int j = 1; j <= nst; j++) {
+                System.out.printf(" %d ", number);
+            }
+
+            System.out.println();
+            nsp--;
+            nst += 2;
+            number++;
+
+        }
     }
 
     public static void p1(int n) {
@@ -22,7 +67,7 @@ public class Main {
             }
 
             System.out.println();
-            nsp --;
+            nsp--;
             nst += 2;
 
         }
