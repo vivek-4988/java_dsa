b4285d45031d787f0d0c9090997e811e1159456b
vivek <vivek@creatingwinners.com>
2022-03-14 04:11:25 +0800

    practice pattern advance

--- day9/src/com/vivekjava/Main.java
+++ day9/src/com/vivekjava/Main.java
@@ -4,7 +4,91 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        p1(5);
+
+//        p1(5);
+        // p2(5);
+        //p3(5);
+        p4(10);
+    }
+
+    public static void p4(int n) {
+        int nsp = n;
+        int nst = 1;
+        int my0Index = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= nsp; j++) {
+                System.out.print("   ");
+            }
+
+            int printN = nsp;
+            for (int j = 1; j <= nst; j++) {
+                if (my0Index == j) {
+                    System.out.print(" 0 ");
+                } else {
+                    System.out.printf(" %d ", printN);
+                    if (j < nst / 2) {
+                        printN++;
+                    } else {
+                        if (j != nst / 2) {
+                            printN--;
+                        }
+                    }
+                }
+            }
+
+            System.out.println();
+            nsp--;
+            nst += 2;
+            my0Index++;
+        }
+
+    }
+
+
+    public static void p3(int n) {
+        int nsp = n - 1;
+        int nst = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= nsp; j++) {
+                System.out.print("   ");
+            }
+
+            for (int j = 1; j <= nst; j++) {
+                if (j == 1 || j == nst) {
+                    System.out.printf(" %d ", i);
+                } else {
+                    System.out.print(" 0 ");
+                }
+            }
+
+            System.out.println();
+            nsp--;
+            nst += 2;
+        }
+
+    }
+
+    public static void p2(int n) {
+        int nsp = n - 1;
+        int nst = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= nsp; j++) {
+                System.out.print("   ");
+            }
+
+            for (int j = 1; j <= nst; j++) {
+                if (j % 2 == 1) {
+                    System.out.print(" * ");
+                } else {
+                    System.out.print(" ! ");
+                }
+            }
+
+            System.out.println();
+            nsp--;
+            nst += 2;
+        }
+
     }
 
     public static void p1(int n) {
