8d313e9f271264b66d06eb940f3a48c58aba852a
vivek <vivek@creatingwinners.com>
2022-03-22 18:43:39 +0800

    inverted hourglass pattern

---
+++ day11/src/com/vivekjava/Main.java
@@ -0,0 +1,56 @@
+package com.vivekjava;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        p3(n);
+    }
+
+    public static void p3(int n) {
+        int myLenth = 2 * n + 1;
+        int nst = 0;
+        int space = myLenth - 2;
+        int secVal = n;
+        for (int i = 1; i <= myLenth; i++) {
+            int myN = n;
+            for (int j = 0; j <= nst; j++) {
+                System.out.printf(" %d ", myN);
+                myN--;
+            }
+
+            for (int j = 1; j <= space; j++) {
+                System.out.printf("   ", space);
+            }
+
+
+            myN = secVal;
+            //nst - 1 is to print 1 less column
+            for (int j = 0; j <= (j == n ? nst - 1 : nst); j++) {
+                System.out.printf(" %d ", myN);
+                myN++;
+            }
+
+
+            System.out.println();
+            if (i <= myLenth / 2) {
+                nst += 1;
+                space -= 2;
+                if (i != n) {
+                    secVal--;
+                }
+            } else {
+                nst -= 1;
+                space += 2;
+                if (i != n + 1) {
+                    secVal++;
+                }
+            }
+        }
+    }
+
+}
