97d14668b2254a5890d5573ec46bee95cf60d3dd
vivek <vivek@creatingwinners.com>
2022-03-29 18:12:26 +0800

    DecimalToanyBase

--- day16/src/com/vivekjava/Main.java
+++ day16/src/com/vivekjava/Main.java
@@ -5,6 +5,22 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         anyBaseToDecimal();
+        DecimalToanyBase();
+    }
+
+    private static void DecimalToanyBase() {
+        int value = 238;
+        int sb = 5;
+        int sum = 0;
+        int multiplier = 1;
+
+        while (value != 0) {
+            int rem = value % sb;
+            value = value / sb;
+            sum = sum + (rem * multiplier);
+            multiplier = multiplier * 10;
+        }
+        System.out.println(sum);
     }
 
     private static void anyBaseToDecimal() {
