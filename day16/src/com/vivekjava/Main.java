19db7a32cb7213355e0b6cc2e38c2b465dea0d11
vivek <vivek@creatingwinners.com>
2022-03-29 18:07:57 +0800

    anyBaseToDecimal

---
+++ day16/src/com/vivekjava/Main.java
@@ -0,0 +1,24 @@
+package com.vivekjava;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        anyBaseToDecimal();
+    }
+
+    private static void anyBaseToDecimal() {
+        int value = 1423;
+        int sb = 5;
+        int sum = 0;
+        int multiplier = 1;
+
+        while (value != 0) {
+            int rem = value % 10;
+            value = value / 10;
+            sum = sum + (rem * multiplier);
+            multiplier = multiplier * sb;
+        }
+        System.out.println(sum);
+    }
+}
