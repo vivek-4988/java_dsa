86a8bb9da5348b0c7685b95061bc8fb450c748a5
vivek <vivek@creatingwinners.com>
2022-04-03 21:26:43 +0800

    FahrenheitToCelsius

--- day19/src/com/vivekjava/Main.java
+++ day19/src/com/vivekjava/Main.java
@@ -5,20 +5,32 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        pascalTriangle(n);
+        FahrenheitToCelsius();
     }
 
-    static void pascalTriangle(int n) {
+    static void FahrenheitToCelsius() {
+        Scanner sc = new Scanner(System.in);
+        long min = sc.nextInt();
+        long max = sc.nextInt();
+        long step = sc.nextInt();
+
+        for (long i = min; i <= max ; i+= step) {
+            long C = ((i - 32) * 5) / 9;
+            System.out.println(i+"\t"+C);
+        }
+    }
+
+    static void pascalTriangle() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j <= i; j++) {
                 int res = 1;
-                int k= j;
+                int k = j;
                 if (i > i - k)
                     k = i - k;
 
-                for (int l = 0; l < k ; l++) {
+                for (int l = 0; l < k; l++) {
                     res *= (i - l);
                     res /= (l + 1);
                 }
