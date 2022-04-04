02d136a6621d6fe71ab23a1eb62b836f0e75823c
vivek <vivek@creatingwinners.com>
2022-04-04 19:30:19 +0800

    print 2d array

--- day20/src/com/vivekjava/Main.java
+++ day20/src/com/vivekjava/Main.java
@@ -1,10 +1,36 @@
 package com.vivekjava;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        wavePrint();
+        print2darray();
+    }
+
+    static void print2darray() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Rows :");
+        int rows = sc.nextInt();
+        int[][] arr = new int[rows][];
+        for (int i = 0; i < rows; i++) {
+            int cols = sc.nextInt();
+            arr[i] = new int[cols];
+            for (int j = 0; j < cols; j++) {
+                System.out.println("enter value for row " + i + "col " + j);
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Display");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
     }
 
     static void wavePrint() {
@@ -14,8 +40,8 @@ public class Main {
                 for (int j = 0; j < array[i].length; j++) {
                     System.out.print(array[i][j] + " ");
                 }
-            }else{
-                for (int j = array[i].length-1; j >= 0 ; j--) {
+            } else {
+                for (int j = array[i].length - 1; j >= 0; j--) {
                     System.out.print(array[i][j] + " ");
                 }
             }
