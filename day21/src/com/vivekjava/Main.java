cf6278eddf713aa4fa6ce3248773e1ddb49afb0d
vivek <vivek@creatingwinners.com>
2022-04-05 20:23:29 +0800

    patternTraingle fibbonaciPattern

--- day21/src/com/vivekjava/Main.java
+++ day21/src/com/vivekjava/Main.java
@@ -8,15 +8,59 @@ public class Main {
         // write your code here
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        mountainPattern(n);
+        patternTraingle(n);
+    }
+
+    static void patternTraingle(int n) {
+        int nsp = n - 1;
+        int nst = 1;
+        int counter;
+        for (int i = 1; i <= n; i++) {
+            counter = i;
+            for (int j = 1; j <= nsp; j++) {
+                System.out.print(" \t");
+            }
+            for (int j = 1; j <= nst; j++) {
+                System.out.print(counter + "\t");
+                if (j > nst / 2) {
+                    counter--;
+                } else {
+                    counter++;
+                }
+            }
+            System.out.println();
+            nst += 2;
+            nsp--;
+        }
+    }
+
+    static void fibbonaciPattern(int n) {
+        int firstEle = 0;
+        int secEle = 1;
+        int sum;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (i == 0) {
+                    System.out.print(firstEle + "\t");
+                } else if (i == 1 && j == 1) {
+                    System.out.print(secEle + "\t");
+                } else {
+                    sum = firstEle + secEle;
+                    firstEle = secEle;
+                    secEle = sum;
+                    System.out.print(sum + "\t");
+                }
+            }
+            System.out.println();
+        }
     }
 
     static void mountainPattern(int n) {
         int nst = 1;
-        int nsp = n + (n-3);
+        int nsp = n + (n - 3);
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= nst; j++) {
-                System.out.print(j+" ");
+                System.out.print(j + " ");
             }
 
             for (int j = 1; j <= nsp; j++) {
@@ -27,7 +71,7 @@ public class Main {
                 nst--;
             }
             for (int j = nst; j >= 1; j--) {
-                System.out.print(j+" ");
+                System.out.print(j + " ");
             }
             System.out.println();
             nsp -= 2;
