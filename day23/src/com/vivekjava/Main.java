3fa4ed666bfab5805ad35a508b0d1a528ea46581
vivek <vivek@creatingwinners.com>
2022-04-10 20:57:17 +0800

    manmohanPattern2

--- day23/src/com/vivekjava/Main.java
+++ day23/src/com/vivekjava/Main.java
@@ -5,7 +5,46 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        manmohanPattern1();
+        manmohanPattern2();
+    }
+
+    static void printBinary() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] arr = new String[n];
+        int i = 0;
+        while (n > 0) {
+            String num = sc.next();
+            arr[i] = num;
+            i++;
+            n--;
+        }
+
+        for (int j = 0; j <= arr.length - 1; j++) {
+            System.out.println(Integer.parseInt(arr[j], 2));
+        }
+
+    }
+
+
+    static void hollowRombusPattern() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int nsp = n - 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= nsp; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            nsp--;
+            System.out.println();
+        }
     }
 
     static void reverse() {
@@ -20,6 +59,26 @@ public class Main {
         System.out.println(reverse);
     }
 
+    static void manmohanPattern2() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int myNum = 1;
+        for (int i = 1; i <= n; i++) {
+            if (i > 2) {
+                myNum++;
+            }
+            for (int j = 1; j <= i; j++) {
+
+                if (i == 2 || j == 1 || j == i) {
+                    System.out.print(myNum);
+                } else {
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+
     //pattern zeros
     static void manmohanPattern1() {
         Scanner sc = new Scanner(System.in);
@@ -30,12 +89,12 @@ public class Main {
                     System.out.print("1");
                 } else {
                     if (i % 2 == 0) {
-                        if (j==1 || j==n){
+                        if (j == 1 || j == n) {
                             System.out.print("1");
-                        }else {
+                        } else {
                             System.out.print("0");
                         }
-                    }else{
+                    } else {
                         System.out.print("1");
                     }
                 }
