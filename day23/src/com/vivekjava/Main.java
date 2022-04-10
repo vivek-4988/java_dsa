34c069efdd9b47e35e7eb1455f28736744e2be87
vivek <vivek@creatingwinners.com>
2022-04-10 20:38:16 +0800

    manmohanPattern1

--- day23/src/com/vivekjava/Main.java
+++ day23/src/com/vivekjava/Main.java
@@ -5,7 +5,43 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        patternZeros();
+        manmohanPattern1();
+    }
+
+    static void reverse() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int temp = n;
+        StringBuilder reverse = new StringBuilder();
+        while (temp > 0) {
+            reverse.append(String.valueOf(temp % 10));
+            temp = temp / 10;
+        }
+        System.out.println(reverse);
+    }
+
+    //pattern zeros
+    static void manmohanPattern1() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || j == i) {
+                    System.out.print("1");
+                } else {
+                    if (i % 2 == 0) {
+                        if (j==1 || j==n){
+                            System.out.print("1");
+                        }else {
+                            System.out.print("0");
+                        }
+                    }else{
+                        System.out.print("1");
+                    }
+                }
+            }
+            System.out.println();
+        }
     }
 
     //pattern zeros
@@ -15,7 +51,7 @@ public class Main {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 if (j == 1 || j == i) {
-                    System.out.print(i+"\t");
+                    System.out.print(i + "\t");
                 } else {
                     System.out.print("0\t");
                 }
