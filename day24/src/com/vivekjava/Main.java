de1a8ae2a8e932406c1b725e00b70584c4647fd8
vivek <vivek@creatingwinners.com>
2022-04-11 12:36:30 +0800

    recursive power,factorial

--- day24/src/com/vivekjava/Main.java
+++ day24/src/com/vivekjava/Main.java
@@ -3,7 +3,26 @@ package com.vivekjava;
 public class Main {
 
     public static void main(String[] args) {
-        pdiskip(5);
+        //pdiskip(5);
+        System.out.println(power(2,5));
+    }
+
+    static int power(int x, int n) {
+        if (n==0){
+            return 1;
+        }
+        int pnm = power(x, n - 1);
+        int pn = x * pnm;
+        return pn;
+    }
+
+    static int fact(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int fnm = fact(n - 1);
+        int fn = n * fnm;
+        return fn;
     }
 
     static void pdiskip(int n) {
