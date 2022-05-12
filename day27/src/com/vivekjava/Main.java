547fd69b7b001383514ad4cf7476c1c7079749da
vivek <vivek@creatingwinners.com>
2022-05-12 10:52:55 +0800

    binary search

--- day27/src/com/vivekjava/Main.java
+++ day27/src/com/vivekjava/Main.java
@@ -9,6 +9,23 @@ public class Main {
     static int[] bArray = {5, 7, 10, 12, 15, 20, 27, 31, 36, 45, 55, 65, 70, 73, 85, 99};
 
     public static void main(String[] args) {
+        System.out.println(bSearch(101));
+    }
+
+    static int bSearch(int item) {
+        int lo = 0;
+        int hi = bArray.length - 1;
+        while (lo <= hi) {
+            int med = (lo + hi) / 2;
+            if (bArray[med] < item) {
+                lo = med + 1;
+            } else if (bArray[med] > item) {
+                hi = med - 1;
+            }else{
+                return med;
+            }
+        }
+        return -1;
     }
 
     static int linearSearch(int item) {
