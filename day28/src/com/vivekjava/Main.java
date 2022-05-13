adf17a4aa65f9a005c2b9d6b3a1fdf94b0719cfa
vivek <vivek@creatingwinners.com>
2022-05-13 21:40:39 +0800

    Kadane's algorithm leetcode_53 max-sum subarray

--- day28/src/com/vivekjava/Main.java
+++ day28/src/com/vivekjava/Main.java
@@ -1,8 +1,30 @@
 package com.vivekjava;
 
 public class Main {
+    //reference took from https://www.enjoyalgorithms.com/blog/maximum-subarray-sum
+
+    static int[] arr = {-4, 5, 7, -6, 10, -15, 3};
+//    int[] arr = {-3, 2, -1, 4, -2};
+//    int[] arr = {5,7,6,10,3};
+
+    static int maxSubArraySum = 0;
 
     public static void main(String[] args) {
-	// write your code here
+
+    }
+
+    //with o(n^2)
+    static void getMaxSubArraySum() {
+        int n = arr.length - 1;
+        for (int i = 0; i < n; i++) {
+            int subArraySum = 0;
+            for (int j = i; j < n; j++) {
+                subArraySum += arr[j];
+                if (subArraySum > maxSubArraySum) {
+                    maxSubArraySum = subArraySum;
+                }
+            }
+        }
+        System.out.println(maxSubArraySum);
     }
 }
